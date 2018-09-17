8283a4f6506b44e762b4bb3d472670110c131d2f
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2018-09-17 16:44:58 +0200

    Implemented first version of Mutalyzer library, added CLI.

--- mutalyzer_client/cli.py
+++ mutalyzer_client/cli.py
@@ -1,21 +1,95 @@
 from argparse import ArgumentParser, FileType, RawDescriptionHelpFormatter
 
+from vcf import Reader
+
 from . import doc_split, usage, version
-from .mutalyzer_client import *
+from .mutalyzer_client import build, Mutalyzer
 
 
-def mutalyzer_client():
+def vcf_to_hgvs(build_name, input_handle, output_handle):
     """
+    Convert all variants in a VCF file to HGVS.
+
+    :arg str build_name: Build name.
+    :arg stream input_handle: Open readable handle to a VCF file.
+    :arg stream output_handle: Open writeable handle to a text file.
     """
-    pass
+    mutalyzer = Mutalyzer(build_name)
+
+    for record in Reader(input_handle):
+        for alt in record.ALT:
+            output_handle.write('{}\n'.format(mutalyzer.vcf_to_hgvs(
+                record.CHROM, record.POS, record.REF, alt)))
+
+
+def vcf_to_details(build_name, input_handle, output_handle):
+    """
+    Convert all variants in a VCF file to details.
+
+    :arg str build_name: Build name.
+    :arg stream input_handle: Open readable handle to a VCF file.
+    :arg stream output_handle: Open writeable handle to a text file.
+    """
+    mutalyzer = Mutalyzer(build_name)
+
+    for record in Reader(input_handle):
+        for alt in record.ALT:
+            output_handle.write('{}\n'.format('\t'.join(mutalyzer.check_genomic(
+                record.CHROM, record.POS, record.REF, alt))))
+
+
+def hgvs_to_details(build_name, input_handle, output_handle):
+    """
+    Convert all HGVS variants in a text file to details.
+
+    :arg str build_name: Build name.
+    :arg stream input_handle: Open readable handle to a text file.
+    :arg stream output_handle: Open writeable handle to a text file.
+    """
+    mutalyzer = Mutalyzer(build_name)
+
+    for record in input_handle.readlines():
+        output_handle.write('{}\n'.format('\t'.join(mutalyzer.hgvs_to_vcf(
+            record))))
 
 
 def main():
     """Main entry point."""
+    build_parser = ArgumentParser(add_help=False)
+    build_parser.add_argument(
+        'build_name', metavar='BUILD', type=str, choices=build.keys(),
+        help='build name')
+
+    input_parser = ArgumentParser(add_help=False)
+    input_parser.add_argument(
+        'input_handle', metavar='INPUT', type=FileType('r'), help='input file')
+
+    output_parser = ArgumentParser(add_help=False)
+    output_parser.add_argument(
+        'output_handle', metavar='OUTPUT', type=FileType('w'),
+        help='output file')
+
     parser = ArgumentParser(
-        formatter_class=RawDescriptionHelpFormatter,
-        description=usage[0], epilog=usage[1])
+        formatter_class=RawDescriptionHelpFormatter, description=usage[0],
+        epilog=usage[1])
     parser.add_argument('-v', action='version', version=version(parser.prog))
+    subparsers = parser.add_subparsers(dest='subcommand')
+    subparsers.required = True
+
+    subparser = subparsers.add_parser(
+        'vcf_to_hgvs', parents=[build_parser, input_parser, output_parser],
+        description=doc_split(vcf_to_hgvs))
+    subparser.set_defaults(func=vcf_to_hgvs)
+
+    subparser = subparsers.add_parser(
+        'vcf_to_details', parents=[build_parser, input_parser, output_parser],
+        description=doc_split(vcf_to_details))
+    subparser.set_defaults(func=vcf_to_details)
+
+    subparser = subparsers.add_parser(
+        'hgvs_to_details', parents=[build_parser, input_parser, output_parser],
+        description=doc_split(hgvs_to_details))
+    subparser.set_defaults(func=hgvs_to_details)
 
     try:
         args = parser.parse_args()
@@ -23,7 +97,7 @@ def main():
         parser.error(error)
 
     try:
-        mutalyzer_client(
+        args.func(
             **{k: v for k, v in vars(args).items()
             if k not in ('func', 'subcommand')})
     except (ValueError, IOError) as error:
---
+++ mutalyzer_client/mutalyzer_client.py
@@ -0,0 +1,101 @@
+from requests import Session
+
+
+build = {
+    'GRCh37': {
+        # https://www.ncbi.nlm.nih.gov/assembly/GCF_000001405.13/
+        'chr1': 'NC_000001.10',
+        'chr2': 'NC_000002.11',
+        'chr3': 'NC_000003.11',
+        'chr4': 'NC_000004.11',
+        'chr5': 'NC_000005.9',
+        'chr6': 'NC_000006.11',
+        'chr7': 'NC_000007.13',
+        'chr8': 'NC_000008.10',
+        'chr9': 'NC_000009.11',
+        'chr10': 'NC_000010.10',
+        'chr11': 'NC_000011.9',
+        'chr12': 'NC_000012.11',
+        'chr13': 'NC_000013.10',
+        'chr14': 'NC_000014.8',
+        'chr15': 'NC_000015.9',
+        'chr16': 'NC_000016.9',
+        'chr17': 'NC_000017.10',
+        'chr18': 'NC_000018.9',
+        'chr19': 'NC_000019.9',
+        'chr20': 'NC_000020.10',
+        'chr21': 'NC_000021.8',
+        'chr22': 'NC_000022.10',
+        'chrX': 'NC_000023.10',
+        'chrY': 'NC_000024.9'},
+    'GRCh38': {
+        # https://www.ncbi.nlm.nih.gov/assembly/GCF_000001405.38
+        'chr1': 'NC_000001.11',
+        'chr2': 'NC_000002.12',
+        'chr3': 'NC_000003.12',
+        'chr4': 'NC_000004.12',
+        'chr5': 'NC_000005.10',
+        'chr6': 'NC_000006.12',
+        'chr7': 'NC_000007.14',
+        'chr8': 'NC_000008.11',
+        'chr9': 'NC_000009.12',
+        'chr10':'NC_000010.11',
+        'chr11':'NC_000011.10',
+        'chr12':'NC_000012.12',
+        'chr13':'NC_000013.11',
+        'chr14':'NC_000014.9',
+        'chr15':'NC_000015.10',
+        'chr16':'NC_000016.10',
+        'chr17':'NC_000017.11',
+        'chr18':'NC_000018.10',
+        'chr19':'NC_000019.10',
+        'chr20':'NC_000020.11',
+        'chr21':'NC_000021.9',
+        'chr22':'NC_000022.11',
+        'chrX': 'NC_000023.11',
+        'chrY': 'NC_000024.10'}}
+
+
+class Mutalyzer(object):
+    def __init__(self, build_name):
+        self._chromosome_to_accession = build[build_name]
+        self._accession_to_chromosome = dict(
+            map(lambda x: x[::-1], self._chromosome_to_accession.items()))
+        self._session = Session()
+
+    def _request(self, **kwargs):
+        return self._session.get(
+            'https://mutalyzer.nl/json/runMutalyzerLight',
+            params={**kwargs, 'extras.varDetails': True}).json()
+
+    def _check_vcf_genomic(self, chromosome, start, ref, alt):
+        return self._request(variant='{}:g.{}_{}delins{}'.format(
+            self._chromosome_to_accession[chromosome],
+            start, start + len(ref) - 1, alt))
+
+    def _check_vcf_transcript(self, chromosome, transcript, start, ref, alt):
+        return self._request(variant='{}({}):c.{}_{}delins{}'.format(
+            self._chromosome_to_accession[chromosome], transcript,
+            start, start + len(ref) - 1, alt))
+
+    def _details_to_vcf(self, details):
+        return (
+            self._accession_to_chromosome[details['reference_file']],
+            details['start'], details['ref'], details['alt'])
+
+    def hgvs_to_vcf(self, variant):
+        return self._details_to_vcf(
+            self._request(variant=variant)['varDetails'])
+
+    def vcf_to_hgvs(self, chromosome, start, ref, alt):
+        return self._check_vcf_genomic(
+            chromosome, start, ref, alt)['genomicDescription']
+
+    def check_genomic(self, chromosome, start, ref, alt):
+        return self._details_to_vcf(
+            self._check_vcf_genomic(chromosome, start, ref, alt)['varDetails'])
+
+    def check_transcript(self, chromosome, transcript, start, ref, alt):
+        return self._details_to_vcf(
+            self._check_vcf_transcript(
+                chromosome, transcript, start, ref, alt)['varDetails'])
